3300f8708d97392961506f8e38daddfd5b898aef
klsh1tt <klsh1tt@gmail.com>
2022-01-24 13:36:01 +0900

    Firebase.initializeApp()できない問題を解消

--- lib/film_list/film_list_model.dart
+++ lib/film_list/film_list_model.dart
@@ -1,5 +1,4 @@
-// import 'package:film_list_sample/domain/film.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:film_stacks/domain/film.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +8,16 @@ class FilmListModel extends ChangeNotifier {
   List<Film>? films;
 
   void fetchFilmList() async {
-    // final QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('films').get();
+    final QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('films').get();
 
-    // final List<Film> films = snapshot.docs.map((DocumentSnapshot document) {
-    //   Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-    //   final String id = document.id;
-    //   final String title = data['title'];
-    //   final String watchingPlatform = data['watchingPlatform'];
-    //   final String? imgURL = data['imgURL'];
-    //   return Film(id, title, watchingPlatform, imgURL);
-    // }).toList();
+    final List<Film> films = snapshot.docs.map((DocumentSnapshot document) {
+      Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+      final String id = document.id;
+      final String title = data['title'];
+      final String watchingPlatform = data['watchingPlatform'];
+      final String? imgURL = data['imgURL'];
+      return Film(id, title, watchingPlatform, imgURL);
+    }).toList();
 
     this.films = films;
     notifyListeners();
